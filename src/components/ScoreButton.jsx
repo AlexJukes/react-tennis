994be03743780a1a63e57e2c61bdc9dd0052590e
Alex <alexanderjukes@gmail.com>
2017-11-27 19:55:38 +0000

    Add score button functionality, but still needs refactor and win conditions to be met.

--- src/components/ScoreButton.jsx
+++ src/components/ScoreButton.jsx
@@ -6,17 +6,9 @@ import PropTypes from 'prop-types'
 
 class ScoreButton extends React.Component {
   render() {
-    return  <button onClick={() => this.props.onScoreClick(player)}>
+    return  <button onClick={() => this.props.onScoreClick(this.props.player)}>
         {this.props.player} score
         </button>
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onScoreClick: (player) => {
-      dispatch(scorePoint(player))
-    }
   }
 }
 
--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -3,27 +3,28 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import ScoreContainer from './containers/ScoreContainer.jsx'
 import ScoreButton from './ScoreButton.jsx'
+import { scorePoint } from '../actions'
+
 
 const player1 = 'player1'
 const player2 = 'player2'
 
-const Scores = ({score}) => (
+const Scores = ({score, onScoreClick}) => (
   <div>
     <div>
       <ScoreContainer player={player1} score={score[player1]} />
         <div>
-          <ScoreButton player={player1} onScoreClick={onScoreClick} />
+          <ScoreButton player={player1} onScoreClick={() => onScoreClick(player1)} />
         </div>
     </div>
     <div>
       <ScoreContainer player={player2} score={score[player2]} />
+      <div>
+        <ScoreButton player={player2} onScoreClick={() => onScoreClick(player2)} />
+      </div>
     </div>
   </div>
 )
-
-export function onScoreClick(dispatch, player) {
-  return dispatch(scorePoint(player))
-}
 
 const mapStateToProps = state => {
   return {
@@ -33,13 +34,15 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onScoreClick: onScoreClick(dispatch, player)
+    onScoreClick: player => {
+      dispatch(scorePoint(player))
+    }
     }
   }
-}
 
 const Scoreboard = connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Scores)
 
 export default Scoreboard
